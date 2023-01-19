e1d06d241dcf53aa72f601850264fcd48c68cf6b
Kamrad33 <comradebender33ru@gmail.com>
2023-01-19 20:31:59 +0300

    added login functionality js

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,52 +1,84 @@
-let userMenuButton = document.getElementById('userMenuButton');
+/*let userMenuButton = document.getElementById('userMenuButton');
 let userMenu = document.getElementById('userMenu');
-
 let pageType = document.getElementsByClassName('content');
+var headerVar = true;
+console.log(mainVar);
+async function mainHeader() {
+    console.log(pageType[0].id);
 
- if ('indexPage' in pageType) {
-    let indexBtn = document.getElementById('indexPg');
-        indexBtn.classList.add('-selected');
- }
- if ('newslistPage' in pageType) {
-    let indexBtn = document.getElementById('newsPg');
-        indexBtn.classList.add('-selected');
- }
- if ('pricePage' in pageType) {
-    let indexBtn = document.getElementById('pricePg');
-        indexBtn.classList.add('-selected');
- }
- if ('mapPage' in pageType) {
-    let indexBtn = document.getElementById('mapPg');
-        indexBtn.classList.add('-selected');
- }
- if ('contactsPage' in pageType) {
-    let indexBtn = document.getElementById('contactsPg');
-        indexBtn.classList.add('-selected');
- }
- if ('favsPage' in pageType) {
-    let indexBtn = document.getElementById('favsPg');
-        indexBtn.classList.add('-selected');
- }
- if ('cathalogPage' in pageType) {
-    let indexBtn = document.getElementById('roomsPg');
-    indexBtn.classList.add('-selected');
-    // extramenu func -------------------------------------------------
- }
- if ('favsPage' in pageType) {
-    let indexBtn = document.getElementById('housesPg');
-        indexBtn.classList.add('-selected');
- }
- if ('favsPage' in pageType) {
-    let indexBtn = document.getElementById('bathsPg');
-        indexBtn.classList.add('-selected');
- }
- if ('favsPage' in pageType) {
-    let indexBtn = document.getElementById('carsPg');
-        indexBtn.classList.add('-selected');
- }
+    switch(pageType[0].id) {
+
+        case 'indexPage': {
+            let indexBtn = document.getElementById('indexPg');
+            indexBtn.classList.add('-selected');
+            console.log('This works 1');
+            break
+        }
+
+        case 'newslistPage': {
+            let indexBtn = document.getElementById('newsPg');
+            indexBtn.classList.add('-selected');
+            console.log('This works 2');
+            break
+        }
+
+        case 'pricePage': {
+            let indexBtn = document.getElementById('pricePg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'mapPage': {
+            let indexBtn = document.getElementById('mapPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'contactsPage': {
+            let indexBtn = document.getElementById('contactsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('favsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'cathalogPage': {
+            let indexBtn = document.getElementById('roomsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('housesPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('bathsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('carsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+    }
+}
+mainHeader();
 
 //get auth tocken-------------------------------------------------------
-let authState = true;
+
+
+
+var authState = true;
 let authBtn = document.getElementById('registerPg');
 let userDiv = document.getElementById('userDiv');
 
@@ -65,3 +97,4 @@ userMenuButton.addEventListener('click', function() {
     menuClass === 'header_Top_user_menu active' ? (userMenu.classList.add('hidden'), console.log('sdsds') ): userMenu.classList.remove('hidden')  
 })
 
+*/
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import '../components';
-
+import '../components/header/header.js';
 let cardsList = [
     {status: 'Gold',photo: '../../assets/images/card_1_Img.png',price: '65.00 BYN',capacity: '4 (2+2)',rooms: '4 комн.',size: '179',address: 'Минск, б-р Мулявина, д. 10',metro: [{icon: 'metro', name: 'Грушевка'}, {icon: 'rail', name: 'Шабаны'}],description: 'Какое-то описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание ...', ownerInfo: {photo: '../../assets/images/boyNextDoorImg.png', name: 'Vasye', phone: ' +375 (29) 291-14-44', email: 'vladimir6234@tut.by'}, type: 'active'},
     {status: 'Gold',photo: '../../assets/images/card_1_Img.png',price: '65.00 BYN',capacity: '4 (2+2)',rooms: '4 комн.',size: '179',address: 'Минск, б-р Мулявина, д. 10',metro: [{icon: 'metro', name: 'Грушевка'}, {icon: 'rail', name: 'Шабаны'}],description: 'Какое-то описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание ...', ownerInfo: {photo: '../../assets/images/boyNextDoorImg.png', name: 'Dimitry', phone: ' +375 (29) 291-14-44', email: 'vladimir6234@tut.by'},type: 'active'},
@@ -9,24 +9,133 @@ let cardsList = [
     {status: 'Gold',photo: '../../assets/images/card_1_Img.png',price: '65.00 BYN',capacity: '4 (2+2)',rooms: '4 комн.',size: '179',address: 'Минск, б-р Мулявина, д. 10',metro: [{icon: 'metro', name: 'Грушевка'}, {icon: 'rail', name: 'Шабаны'}],description: 'Какое-то описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание квартиры, описание ...', ownerInfo: {photo: '../../assets/images/boyNextDoorImg.png', name: 'Dimitry', phone: ' +375 (29) 291-14-44', email: 'vladimir6234@tut.by'},type: 'active'},
     ];
 
+
+let userMenuButton = document.getElementById('userMenuButton');
+let userMenu = document.getElementById('userMenu');
 let pageType = document.getElementsByClassName('content');
-if ('cathalogPage' in pageType) {
-    let menuExtra = document.getElementById('menuExtra');
-    let menuExtraButton = document.getElementById('extraMenuButton');      
-    menuExtraButton.addEventListener('click', function() {
-    let menuClass = menuExtra.className;
-    menuClass === 'content_container_searchBlockCathalogFilterContainer_innerContainer_menuExtra active' ? menuExtra.classList.add('hidden') : menuExtra.classList.remove('hidden')  
-});
+var authState = false;
+async function mainHeader() {
+    console.log(pageType[0].id);
+
+    switch(pageType[0].id) {
+
+        case 'indexPage': {
+            let indexBtn = document.getElementById('indexPg');
+            indexBtn.classList.add('-selected');
+            console.log('This works 1');
+            break
+        }
+
+        case 'newslistPage': {
+            let indexBtn = document.getElementById('newsPg');
+            indexBtn.classList.add('-selected');
+            console.log('This works 2');
+            break
+        }
+
+        case 'pricePage': {
+            let indexBtn = document.getElementById('pricePg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'mapPage': {
+            let indexBtn = document.getElementById('mapPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'contactsPage': {
+            let indexBtn = document.getElementById('contactsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('favsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'cathalogPage': {
+            let indexBtn = document.getElementById('roomsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('housesPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('bathsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+        case 'favsPage': {
+            let indexBtn = document.getElementById('carsPg');
+            indexBtn.classList.add('-selected');
+            break
+        }
+
+    }
 }
 
-checkPassword = (password) => {
-    //min 8 letter password, with at least a symbol, upper and lower case letters and a number
-    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-    return re.test(password); 
-}
+async function main() {
+    
+    
+    switch(pageType[0].id) {
 
+        //login page logic---------------------
+        case 'loginPage': {
 
-if('registerPage' in pageType) {
+            let loginBtn = document.getElementById('loginBtn');
+            let loginInput = document.getElementById('login');
+            let passwordInput = document.getElementById('password');
+
+            loginBtn.addEventListener('click', function () {
+                //console.log(loginInput.value);
+                //console.log(passwordInput.value);
+                console.log(authState);
+                getAuth(loginInput.value, passwordInput.value);            
+            });
+
+            async function getAuth (login, password) {
+                const response = await fetch('https://jsonplaceholder.typicode.com/users')
+                const authData = await response.json();
+                console.log('RES', authData);
+
+                for (let key in authData) {
+                    console.log(authData[key]);
+
+                    if (login === authData[key].username){
+                        authState = true;
+                        checkAuth(authState);
+                        return console.log('auth', login, password, authState);
+                    } else {
+                        return console.log('false');
+                    }
+                }                   
+            }       
+           break
+        }
+
+        //cathalog page logic---------------------
+        case 'cathalogPage': {
+            let menuExtra = document.getElementById('menuExtra');
+            let menuExtraButton = document.getElementById('extraMenuButton');      
+            menuExtraButton.addEventListener('click', function() {
+            let menuClass = menuExtra.className;
+            menuClass === 'content_container_searchBlockCathalogFilterContainer_innerContainer_menuExtra active' ? menuExtra.classList.add('hidden') : menuExtra.classList.remove('hidden')  
+        })
+        break
+        }
+
+        //register page logic---------------------
+        case 'registerPage': {
     let regBtn = document.getElementById('registerBtn');
     let alertState = document.getElementById('alert');
     let loginInput = document.getElementById('login');
@@ -76,10 +185,42 @@ if('registerPage' in pageType) {
             alertState.classList.remove("wrong")
         }
     })   
+        }
+    }
+}
+
+main();    
+mainHeader();
+checkAuth();
+
+async function checkAuth (state) {
+
+    let authBtn = document.getElementById('registerPg');
+    let userDiv = document.getElementById('userDiv');
+
+    if (state) {  
+        authBtn.classList.replace('primary', 'hidden');
+        userDiv.classList.replace('hidden', 'visible');
+    }
+    else {
+        userDiv.classList.replace('visible', 'hidden');
+        authBtn.classList.replace('hidden', 'primary');   
+    }
+
+    userMenuButton.addEventListener('click', function() {
+        let menuClass = userMenu.className;
+        menuClass === 'header_Top_user_menu active' ? (userMenu.classList.add('hidden'), console.log('sdsds') ): userMenu.classList.remove('hidden')  
+    })
 }
 
 
- 
+checkPassword = (password) => {
+    //min 8 letter password, with at least a symbol, upper and lower case letters and a number
+    var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
+    return re.test(password); 
+}
+
+
 let cardDiv = document.querySelector('#cardsList');
 let items = document.querySelectorAll('#pagination li');
 let cardsOnPage = 6;
